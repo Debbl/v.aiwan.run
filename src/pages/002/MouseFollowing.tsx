67bc1de646dcb01d3858b32a68bb55790ae71e3f
Debbl <me@aiwan.run>
2022-10-18 21:30:11 +0800

    feat: mouse following

---
+++ src/pages/002/MouseFollowing.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useRef } from "react";
+import Particle from "./utils/Particle";
+
+export interface Cursor {
+  x: number;
+  y: number;
+}
+
+function MouseFollowing() {
+  const containerEl = useRef<HTMLDivElement>(null);
+  const cEl = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const ctx = cEl.current!.getContext("2d")!;
+    const { clientWidth, clientHeight } = containerEl.current!;
+    const particlesArray: Particle[] = [];
+    const cursor: Cursor = {
+      x: clientWidth / 2,
+      y: clientHeight / 2,
+    };
+    function main() {
+      containerEl.current!.addEventListener<"mousemove">("mousemove", (e) => {
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+      });
+      containerEl.current!.addEventListener(
+        "touchmove",
+        (e) => {
+          e.preventDefault();
+          cursor.x = e.touches[0].clientX;
+          cursor.y = e.touches[0].clientY;
+        },
+        { passive: false }
+      );
+      generateParticles(101);
+      requestAnimationFrame(anim);
+    }
+    function anim() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+      ctx.fillRect(0, 0, cEl.current!.clientWidth, cEl.current!.clientHeight);
+      particlesArray.forEach((p) => p.rotate(cursor, ctx));
+      requestAnimationFrame(anim);
+    }
+    function generateParticles(amount: number) {
+      Array.from({ length: amount }).forEach((_, i) => {
+        particlesArray[i] = new Particle(
+          clientWidth / 2,
+          clientHeight / 2,
+          4,
+          generateColor(),
+          0.02
+        );
+      });
+    }
+    function generateColor() {
+      const hexSet = "0123456789ABCDEF";
+      let finalHexString = "#";
+      for (let i = 0; i < 6; i++) {
+        finalHexString += hexSet[Math.floor(Math.random() * 16)];
+      }
+      return finalHexString;
+    }
+    main();
+  }, []);
+  return (
+    <div ref={containerEl} className="border">
+      <canvas ref={cEl} width="600" height="400" className=""></canvas>
+    </div>
+  );
+}
+export default MouseFollowing;
---
+++ src/pages/002/utils/Particle.ts
@@ -0,0 +1,43 @@
+import type { Cursor } from "../MouseFollowing";
+
+class Particle {
+  x: number;
+  y: number;
+  particleTrailWidth: number;
+  strokeColor: string;
+  rotateSpeed: number;
+  t: number = Math.random() * 150;
+  theta: number = Math.random() * Math.PI * 2;
+  constructor(
+    x: number,
+    y: number,
+    particleTrailWidth: number,
+    strokeColor: string,
+    rotateSpeed: number
+  ) {
+    this.x = x;
+    this.y = y;
+    this.particleTrailWidth = particleTrailWidth;
+    this.strokeColor = strokeColor;
+    this.rotateSpeed = rotateSpeed;
+  }
+
+  rotate = (cursor: Cursor, context: CanvasRenderingContext2D) => {
+    const ls = {
+      x: this.x,
+      y: this.y,
+    };
+    this.theta += this.rotateSpeed;
+    this.x = cursor.x + Math.cos(this.theta) * this.t;
+    this.y = cursor.y + Math.sin(this.theta) * this.t;
+
+    context.beginPath();
+    context.lineWidth = this.particleTrailWidth;
+    context.strokeStyle = this.strokeColor;
+    context.moveTo(ls.x, ls.y);
+    context.lineTo(this.x, this.y);
+    context.stroke();
+  };
+}
+
+export default Particle;
