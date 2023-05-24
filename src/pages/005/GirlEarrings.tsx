31ff865939ffe3de47f18df55a451e0da61c1b92
Debbl <me@aiwan.run>
2023-05-24 21:16:35 +0800

    update: 005 change environment map

--- src/pages/005/GirlEarrings.tsx
+++ src/pages/005/GirlEarrings.tsx
@@ -1,14 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
-import girlEarringsModel from "./assets/girl-earrings.glb";
-import envHDR from "./assets/050.hdr";
+import nx from "./assets/nx.jpg";
+import nz from "./assets/nz.jpg";
+import ny from "./assets/ny.jpg";
+import px from "./assets/px.jpg";
+import py from "./assets/py.jpg";
+import pz from "./assets/pz.jpg";
 
 const GirlEarrings: React.FC = () => {
   const girlEarringsEl = useRef<HTMLDivElement>(null);
+  const [loaded, setLoaded] = useState(0);
 
   useEffect(() => {
     const el = girlEarringsEl.current!;
@@ -23,10 +27,36 @@ const GirlEarrings: React.FC = () => {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("./draco/");
     const gltfLoader = new GLTFLoader().setDRACOLoader(dracoLoader);
-    gltfLoader.load(
-      girlEarringsModel,
-      (m) => {
-        scene.add(m.scene);
+    import("./assets/girl-earrings.glb").then((r) => {
+      gltfLoader.load(
+        r.default,
+        (m) => {
+          scene.add(m.scene);
+        },
+        (progress) => {
+          setLoaded(progress.loaded);
+          // eslint-disable-next-line no-console
+          console.log(progress);
+        },
+        (err) => {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        },
+      );
+    });
+
+    // Light
+    const directionLight = new THREE.DirectionalLight("#ffffff", 3);
+    directionLight.position.set(0, 3, 2.25);
+    scene.add(directionLight);
+
+    // env
+    const cubeTextureLoader = new THREE.CubeTextureLoader();
+    cubeTextureLoader.load(
+      [px, nx, py, ny, pz, nz],
+      (t) => {
+        scene.background = t;
+        scene.environment = t;
       },
       (progress) => {
         // eslint-disable-next-line no-console
@@ -37,19 +67,6 @@ const GirlEarrings: React.FC = () => {
         console.log(err);
       },
     );
-
-    // Light
-    const directionLight = new THREE.DirectionalLight("#ffffff", 3);
-    directionLight.position.set(0, 3, 2.25);
-    scene.add(directionLight);
-
-    // env
-    const hdrLoader = new RGBELoader();
-    hdrLoader.loadAsync(envHDR).then((t) => {
-      t.mapping = THREE.EquirectangularReflectionMapping;
-      scene.background = t;
-      scene.environment = t;
-    });
 
     // render
     const renderer = new THREE.WebGLRenderer({
@@ -78,7 +95,18 @@ const GirlEarrings: React.FC = () => {
     }
     requestAnimationFrame(render);
   }, []);
-  return <div ref={girlEarringsEl} className="fixed inset-0"></div>;
+  return (
+    <>
+      <div ref={girlEarringsEl} className="fixed inset-0"></div>
+      {loaded !== 86328 && (
+        <div>
+          <div className="fixed inset-0 flex justify-center items-center bg-white">
+            loading {((loaded / 86328) * 100).toFixed(0)}%
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default GirlEarrings;
