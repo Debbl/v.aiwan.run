d11729f31ce7664cc1c30207db2b2c7bb451c763
Debbl <me@aiwan.run>
2023-06-22 15:51:57 +0800

    feat: add fn input

--- src/pages/006/Tin.tsx
+++ src/pages/006/Tin.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import P5 from "p5";
 import colors from "./assets/colors";
 import Dot from "./composables/Dot";
 import { useDark } from "./hooks/useDark";
+import { getMathFn } from "./utils";
+
+export type MathFn = (t: number, i: number, x: number) => number;
 
 const ITEMS_COUNT = 32;
 
-const animationPlay = (el: HTMLElement, bgColor: string) => {
+const animationPlay = (el: HTMLElement, bgColor: string, mathFn: MathFn) => {
   const dots: Dot[] = [];
   let time = 0;
   const sketch = (s: P5) => {
@@ -25,7 +28,7 @@ const animationPlay = (el: HTMLElement, bgColor: string) => {
 
       for (let i = 0; i < dots.length; i++) {
         const x = dots[i].getX();
-        const value = Math.sin(x / 4 + time);
+        const value = mathFn(time, i, x);
 
         dots[i].setValue(value);
       }
@@ -50,22 +53,44 @@ const animationPlay = (el: HTMLElement, bgColor: string) => {
 
 const Tin: React.FC = () => {
   const elRef = useRef<HTMLDivElement>(null);
+  const [fn, setFn] = useState("Math.sin(t + i + x)");
   const [isDark] = useDark();
 
   const currentTheme = isDark ? "dark" : "light";
   const bgTheme = currentTheme === "light" ? "bg-[#fafafa]" : "bg-black";
 
+  const mathFn: MathFn = useMemo(() => {
+    const _fn = getMathFn("t, i, x", fn);
+
+    return (t: number, i: number, x: number) => {
+      try {
+        return _fn(t, i, x);
+      } catch (e) {
+        return 1;
+      }
+    };
+  }, [fn]);
+
   useEffect(() => {
-    const p5 = animationPlay(elRef.current!, colors[currentTheme].background);
+    const p5 = animationPlay(elRef.current!, colors[currentTheme].background, mathFn);
 
     return () => {
       p5 && p5.remove();
     };
-  }, [currentTheme]);
+  }, [currentTheme, mathFn]);
 
   return (
-    <div className={`fixed flex h-screen w-screen justify-center p-10 ${bgTheme}`}>
+    <div className={`fixed flex h-screen w-screen flex-col items-center p-10 ${bgTheme}`}>
       <div ref={elRef}></div>
+      <div className="mt-6 flex flex-col items-start gap-y-2">
+        <div className="opacity-30">{"(t, i, x) => "}</div>
+        <input
+          type="text"
+          className="w-96 border-b  border-gray-700 bg-transparent p-2 outline-none"
+          value={fn}
+          onChange={(e) => setFn(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
